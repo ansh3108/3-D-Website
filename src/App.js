5586c70f0a143a91271779699180da9aa8d7c61c
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-22 16:29:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled, { ThemeProvider } from "styled-components";
+import {darkTheme} from "./utils/Themes"
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}> 
+    <Navbar />
+     </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from "styled-components"
+
+
+const NavbarContainer = styled.div``
+
+const Navbar = () => {
+  return (
+    <NavbarContainer> Navbar </NavbarContainer>
+  )
+}
+
+export default Navbar;
